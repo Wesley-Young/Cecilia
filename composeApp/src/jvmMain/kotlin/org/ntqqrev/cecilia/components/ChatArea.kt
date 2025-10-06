cb6664f80fdce4fb9b78660328ec94d166fb09f1
Wesley F. Young <wesley.f.young@outlook.com>
2025-10-06 22:50:19 +0800

    fix: remove deprecated usage

--- composeApp/src/jvmMain/kotlin/org/ntqqrev/cecilia/components/ChatArea.kt
+++ composeApp/src/jvmMain/kotlin/org/ntqqrev/cecilia/components/ChatArea.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -164,7 +165,7 @@ private fun ChatInputArea(
                 backgroundColor = MaterialTheme.colors.primary
             ) {
                 Icon(
-                    imageVector = Icons.Default.Send,
+                    imageVector = Icons.AutoMirrored.Filled.Send,
                     contentDescription = "发送",
                     tint = MaterialTheme.colors.onPrimary
                 )
