77373c945f929b654f9c83bf63d5a33bcabf2033
Wei FeiYang <wesley.f.young@outlook.com>
2025-11-17 09:56:45 +0800

    feat: add handling for replied messages in segment preview

--- composeApp/src/jvmMain/kotlin/org/ntqqrev/cecilia/util/SegmentPreviewUtils.kt
+++ composeApp/src/jvmMain/kotlin/org/ntqqrev/cecilia/util/SegmentPreviewUtils.kt
@@ -12,6 +12,7 @@ fun BotIncomingMessage.segmentsToPreviewString(): String {
             is BotIncomingSegment.MarketFace
                 if segment.url == "https://gxh.vip.qq.com/club/item/parcel/item/a3/a322b9f5bf8e2f0f2370d933f6fd4239/raw300.gif" -> "[逼？！]"
             // easter egg
+            is BotIncomingSegment.Reply -> "[回复消息]" // hide replied seq
             else -> segment.toString()
         }
     }.take(100) // 限制预览长度
